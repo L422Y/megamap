181037f6e00b979bfc81052ed1dd7c048e86375e
Larry W. <l422y@l422y.com>
2023-11-11 13:05:39 -0500

    Add CacheItem and FilterableMapOptions types in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+type CacheItem<V> = {
+    value: V,
+    expiry: number
+}
+
+type FilterableMapOptions<K, V> = {
+    filter?: (item: V) => boolean;
+    sort?: (item1: V, item2: V) => number;
+    searchableFields?: ( keyof V )[];
+}
+
+
 export * from "./CachedLoadableMap"
 export * from "./LoadableMap"
 export * from "./MegaMap"
