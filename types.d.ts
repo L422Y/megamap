532447795a28ecea792d362d8a124f994b9b808c
Larry W. <l422y@l422y.com>
2023-11-11 09:23:36 -0500

    Add CacheItem and FilterableMapOptions types in types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,11 @@
+type CacheItem<V> = {
+    value: V,
+    expiry: number
+}
+
+
+type FilterableMapOptions<K, V> = {
+    filter?: (item: V) => boolean;
+    sort?: (item1: V, item2: V) => number;
+    searchableFields?: (keyof V)[];
+};
