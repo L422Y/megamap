aca0dff9d51f856708c8b6369b7807d05fb07bcf
Larry W. <l422y@l422y.com>
2023-11-11 17:53:19 -0500

    Update LoadableMap tests to cover new logic and error handling

--- tests/LoadableMap.test.ts
+++ tests/LoadableMap.test.ts
@@ -1,28 +1,48 @@
-import { LoadableMap } from "../src/LoadableMap"
+import { LoadableMap } from "../src"
 
 describe("LoadableMap", () => {
-    let loadableMap: LoadableMap<string, string>
-    let mockLoadFunction: jest.Mock
+    let loadableMap: LoadableMap<string, { _id: string, data: string }>
+    let mockLoadOneFunction: jest.Mock
     let mockLoadAllFunction: jest.Mock
 
     beforeEach(() => {
-        mockLoadFunction = jest.fn(key => Promise.resolve(`Value for ${key}`))
-        mockLoadAllFunction = jest.fn(() => Promise.resolve(new Map([
-            ["key1", "value1"],
-            ["key2", "value2"]
-        ])))
-        loadableMap = new LoadableMap(mockLoadFunction, mockLoadAllFunction)
+        mockLoadOneFunction = jest.fn().mockImplementation((key) => Promise.resolve({
+            _id: key,
+            data: `Data for ${key}`
+        }))
+        mockLoadAllFunction = jest.fn().mockImplementation(() => Promise.resolve([
+            {_id: "key1", data: "value1"},
+            {_id: "key2", data: "value2"}
+        ]))
+        loadableMap = new LoadableMap({
+            loadOne: mockLoadOneFunction,
+            loadAll: mockLoadAllFunction,
+            keyProperty: "_id"
+        })
     })
 
-    test("should call load function on get", async () => {
-        await loadableMap.get("testKey")
-        expect(mockLoadFunction).toHaveBeenCalledWith("testKey")
+    test("should call loadOne and add item to map if not present", async () => {
+        const item = await loadableMap.get("key1")
+        expect(mockLoadOneFunction).toHaveBeenCalledWith("key1")
+        expect(item).toEqual({_id: "key1", data: "Data for key1"})
+        expect(loadableMap["_map"].has("key1")).toBeTruthy()
     })
 
-    test("should call loadAll function on getAll", async () => {
+    test("should return undefined for nonexistent key", async () => {
+        mockLoadOneFunction.mockResolvedValueOnce(undefined)
+        const item = await loadableMap.get("nonexistent")
+        expect(item).toBeUndefined()
+    })
+
+    test("should throw error for undefined key", async () => {
+        await expect(loadableMap.get(undefined!)).rejects.toThrow("Key is undefined")
+    })
+
+    test("should load all items and add them to map", async () => {
         const allItems = await loadableMap.getAll()
         expect(mockLoadAllFunction).toHaveBeenCalled()
-        expect(allItems).toBeInstanceOf(Map)
         expect(allItems?.size).toBe(2)
+        expect(allItems?.get("key1")).toEqual({_id: "key1", data: "value1"})
+        expect(allItems?.get("key2")).toEqual({_id: "key2", data: "value2"})
     })
 })
