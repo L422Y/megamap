e2bd8c787362155c261993979621d9434b19b64a
Larry W. <l422y@l422y.com>
2023-11-12 15:33:16 -0500

    Update MegaMap class to include new features and optimizations

--- src/MegaMap.ts
+++ src/MegaMap.ts
@@ -1,37 +1,90 @@
 import { CachedLoadableMap } from "./CachedLoadableMap"
-
+import type { Ref, ShallowRef } from "vue"
+import { isRef, reactive, ref, triggerRef } from "vue"
+import Fuse from "fuse.js"
 
 interface MegaMapOptions<K, V> {
-    loadOne: (key: K) => Promise<V | undefined>,
-    loadAll?: () => Promise<Map<K, V> | V[]>,
-    expiryInterval?: number
-    keyProperty?: keyof V
+    loadOne: (key: string) => Promise<V | undefined>,
+    loadAll?: () => Promise<Map<string, V> | V[]>,
+    expiryInterval?: number,
+    keyProperty?: string,
+    secondaryMaps?: Array<MegaMap<any, any> | Ref<MegaMap<any, any>>>,
+    filter?: (item: V) => boolean,
+    sort?: (item1: V, item2: V) => number,
+    searchableFields?: string[]
+    subListFilters?: Record<string, (item: V) => boolean>;
+    onUpdated?: () => void
 }
 
-export class MegaMap<K, V extends Record<string, any>> extends CachedLoadableMap<K, V> {
-    private secondaryMaps: Array<MegaMap<any, any>> = []
 
-    constructor({loadOne, loadAll, expiryInterval, keyProperty = "_id" as keyof V}: MegaMapOptions<K, V>) {
-        super({loadOne, loadAll, expiryInterval, keyProperty})
-        this.keyProperty = keyProperty as keyof V || "_id" as keyof V
+export class MegaMap<K, V extends Record<string, any>> extends CachedLoadableMap<string, V> {
+    readonly [Symbol.toStringTag]: string = "MegaMap"
+    public readonly subLists: Record<string, V[]>
+    protected _triggerRef?: Function
+    private secondaryMaps: Array<MegaMap<string, V> | Ref<MegaMap<string, V>>> = []
+    private readonly _filter?: (item: V) => boolean
+    private readonly _sort?: (item1: V, item2: V) => number
+    private readonly _searchableFields: string[]
+    private readonly _subListFilters: Record<string, (item: V) => boolean>
+    private _refInstance: Ref<MegaMap<K, V>>
+
+    constructor(opts: MegaMapOptions<string, V>) {
+        super(opts)
+        this.secondaryMaps = opts.secondaryMaps || []
+        this._filter = opts.filter
+        this._sort = opts.sort
+        this._searchableFields = opts.searchableFields ?? []
+        this._subListFilters = opts.subListFilters || {}
+        this.subLists = {}
+        this.onUpdated = opts.onUpdated
+
+
+        for (const filterKey in this._subListFilters) {
+            this.subLists[filterKey] = ref([])
+        }
+
+        this.getAll().then(() => {
+            this.updateSubLists()
+        })
+
+        if (this.onUpdated) {
+            setTimeout(() => {
+                this.onUpdated()
+            }, 0)
+        }
     }
 
-    public get size(): number {
-        return super.size
+    public getRefInstance(): MegaMap<K, V> {
+        if (!this._refInstance) this._refInstance = reactive(this)
+        return this._refInstance
     }
 
-    public async get(key: K): Promise<V | undefined> {
-        const value = await super.get(key)
-        this.secondaryMaps.forEach(map => map.set(key, value))
-        return value
+    // static ReactiveMegaMap = (opts: MegaMapOptions<any, any>): ShallowRef<MegaMap<any, any>> => {
+    //     const megaMap = shallowRef(new MegaMap(opts))
+    //     megaMap.onUpdated = () => {
+    //         triggerRef(megaMap)
+    //         opts.onUpdated?.()
+    //     }
+    //     return megaMap
+    // }
+
+    public async get(key: string): Promise<V | undefined> {
+        return await super.get(key).then((result: V | undefined) => {
+            if (result) {
+                this.updateSecondaryMaps(result)
+                this.updateSubLists()
+
+            }
+            return result
+        })
     }
 
-    public async getAll(): Promise<Map<K, V>> {
+    public async getAll(): Promise<Map<string, V>> {
         const items = await super.getAll()
         if (!items) {
             throw new Error("No items found")
         }
-
+        this.updateSubLists()
         return items
     }
 
@@ -39,12 +92,114 @@ export class MegaMap<K, V extends Record<string, any>> extends CachedLoadableMap
         items.forEach(item => {
             this.set(item[this.keyProperty], item)
         })
-        this.secondaryMaps.forEach(map => map.bulkAdd(items))
+        this.secondaryMaps.forEach(map => {
+            if (isRef(map)) {
+                map.value.bulkAdd(items)
+            } else {
+                map.bulkAdd(items)
+            }
+        })
     }
 
-    public async addSecondaryMap(map: MegaMap<any, any>) {
+    public async addSecondaryMap(map: MegaMap<string, V> | Ref<MegaMap<string, V>>) {
         this.secondaryMaps.push(map)
-        await map.bulkAdd(Array.from(this._map.values()))
+        if (isRef(map)) {
+            await map.value.bulkAdd(Array.from(this._map.values()))
+        } else {
+            await map.bulkAdd(Array.from(this._map.values()))
+        }
     }
 
+    public filterItems(criteria: (value: V) => boolean): V[] {
+        return [...this.values()].filter((value) => criteria(value))
+    }
+
+    public sortItems(comparator: (a: V, b: V) => number): V[] {
+        return [...this.values()].sort(comparator)
+    }
+
+    public searchItems(query: string): V[] {
+        let results: V[] = []
+        // first try exact matches
+        results = [...this.values()].filter((item: V) => {
+            for (const field of this._searchableFields) {
+                if (field in item) {
+                    const val = item[field as keyof typeof item] as string
+                    if (val.toLowerCase().includes(query.toLowerCase())) {
+                        return true
+                    }
+                }
+            }
+            return false
+        })
+
+        // if no exact matches, do fuzzy search
+        if (results.length === 0) {
+            const fuse = new Fuse(Array.from(this.values()), {
+                keys: this._searchableFields,
+                threshold: 0.5
+            })
+            try {
+                results = fuse.search(query).map(result => result.item)
+            } catch (err) {
+                console.error(err)
+                results = []
+            }
+        }
+
+        return results
+    }
+
+
+    // SUB LISTS
+
+    public set(key: string, value: V): this {
+        super.set(key, value)
+        this.notifyMapUpdated()
+        return this
+    }
+
+    public delete(key: string): boolean {
+        const result = super.delete(key)
+        this.notifyMapUpdated()
+        return result
+    }
+
+    public clear(): void {
+        super.clear()
+        this.notifyMapUpdated()
+    }
+
+    private updateSecondaryMaps(item: V) {
+        this.secondaryMaps.forEach(map => {
+            if (isRef(map)) {
+                map.value.set(item[map.value.keyProperty], item)
+            } else if (map instanceof MegaMap) {
+                map.set(item[map.keyProperty], item)
+            }
+        })
+    }
+
+    private updateSubLists() {
+        for (const [filterKey, filter] of Object.entries(this._subListFilters)) {
+            this.subLists[filterKey].value = [...this.values()].filter(filter)
+            triggerRef(this.subLists[filterKey])
+        }
+    }
+
+    private notifyMapUpdated() {
+        this.updateSubLists()
+        this.secondaryMaps.forEach(map => {
+            if (isRef(map)) {
+                map.value.notifyMapUpdated()
+            } else if (map instanceof MegaMap) {
+                map.notifyMapUpdated()
+            }
+        })
+        triggerRef(this._refInstance)
+        if (this.onUpdated) {
+            super.onUpdated()
+            this.onUpdated()
+        }
+    }
 }
