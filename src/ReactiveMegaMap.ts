5a6853bca1b6b9e4594a3c494186a041f3e31e7c
Larry W. <l422y@l422y.com>
2023-11-14 15:25:10 -0500

    Refactor ReactiveMegaMap to use MaybeRef for subLists and streamline initialization

--- src/ReactiveMegaMap.ts
+++ src/ReactiveMegaMap.ts
@@ -1,45 +1,35 @@
 import { MegaMap, MegaMapOptions } from "./MegaMap"
 
-import { reactive } from "vue"
+import { MaybeRef, reactive } from "vue"
 
 export class ReactiveMegaMap<K, V extends Record<string, any>> extends MegaMap<string, V> {
     readonly [Symbol.toStringTag]: string = "ReactiveMegaMap"
+    _subLists: MaybeRef<Record<string, V[]>> = reactive({})
+
+    get subLists(): MaybeRef<Record<string, V[]>> {
+        return this._subLists
+    }
+
+    set subLists(value: MaybeRef<Record<string, V[]>>) {
+        this._subLists = value
+        // triggerRef(this._subLists)
+    }
 
     async init(opts: MegaMapOptions<string, V>) {
+
         this._map = reactive(this._map)
-        this.subLists = new Proxy(reactive({}), {
-            get: (target, prop) => {
-                if (prop === Symbol.iterator) {
-                    return function* () {
-                        for (let [key, val] of Object.entries(target)) {
-                            yield [key, val]
-                        }
-                    }
-                }
-                return prop in target ? target[prop] : []
-            },
-            set: (target, prop, value) => {
-                target[prop] = value
-                return true
-            }
-        })
-
-
-        this.onUpdated = opts.onUpdated
+        this._subLists = reactive({})
 
         for (const filterKey in this._subListFilters) {
-            this.subLists[filterKey] = []
+            this._subLists[filterKey] = []
         }
 
-        this.getAll().then(() => {
-            this.updateSubLists()
-        })
+        this.on("updated", () => this.updateSubLists())
+    }
 
-        if (this.onUpdated) {
-            setTimeout(() => {
-                this.notifyMapUpdated()
-                this.onUpdated()
-            }, 0)
+    updateSubLists() {
+        for (const [filterKey, filter] of Object.entries(this._subListFilters)) {
+            this._subLists[filterKey] = [...this.values()].filter(filter)
         }
     }
 
