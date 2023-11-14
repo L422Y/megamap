3551b165d7062de0f79b2368a41369c02c729b69
Larry W. <l422y@l422y.com>
2023-11-14 15:24:55 -0500

    Add cache query handling to CachedLoadableMap class

--- src/CachedLoadableMap.ts
+++ src/CachedLoadableMap.ts
@@ -5,20 +5,69 @@ type CacheExpiry = {
 };
 
 export type CachedLoadableMapOptions<K, V> = {
-    loadOne: (key: string) => Promise<V | undefined>;
-    loadAll?: () => Promise<V[] | Map<string, V>>;
-    expiryInterval?: number;
-    keyProperty?: string;
+    loadOne: (key: string) => Promise<V | undefined>
+    loadAll?: () => Promise<V[] | Map<string, V>>
+    expiryInterval?: number
+    keyProperty?: string
 } & LoadableMapOptions<K, V>
 
 export class CachedLoadableMap<K, V extends Record<string, any>> extends LoadableMap<string, V> {
     [Symbol.toStringTag]: string = "CachedLoadableMap"
     private expiryCache: Map<string, CacheExpiry> = new Map<string, CacheExpiry>()
     private readonly expiryInterval?: number
+    private _cachedQueryExecutors: Record<string, (...args: any[]) => Promise<V | V[]>> = {}
+
+    private _cachedQueryData: Map<string, V | V[]> = new Map<string, V | V[]>()
 
     constructor(opts: CachedLoadableMapOptions<string, V>) {
         super(opts)
         this.expiryInterval = opts.expiryInterval
+
+
+        for (const queryName in this._namedQueries) {
+            this._cachedQueryExecutors[queryName] = async (...args: any[]) => this.executeCachedQuery(queryName, ...args)
+        }
+
+    }
+
+    get query(): Record<string, (...args: any[]) => Promise<V | V[]>> {
+        return this._cachedQueryExecutors
+    }
+
+    async executeCachedQuery(queryName: string, ...args: any[]): Promise<V | V[]> {
+
+        const queryKey = `${queryName}:${args.join(":")}`
+
+
+        if (this.loadingQuery.has(queryKey)) {
+            // LOADING / PENDING
+            return this.loadingQuery.get(queryKey)
+        }
+
+        if (this._cachedQueryData.has[queryKey]) {
+            // HIT
+            const cacheExpiry = this.expiryCache.get(queryKey)
+            const now = Date.now()
+
+            if (cacheExpiry) {
+                if (cacheExpiry.expiry === -1 || now < cacheExpiry.expiry) {
+                    // NOT EXPIRED
+                    return this._cachedQueryData.get(queryKey)
+                } else {
+                    // EXPIRED
+                    this.expiryCache.delete(queryKey)
+                    this.loadingQuery.delete(queryKey)
+                }
+            }
+        }
+        // MISS
+        const promise = this._queryExecutors[queryName](queryName, ...args).then((result: V | V[]) => {
+            this._cachedQueryData[queryKey] = result
+            return result
+        })
+
+        this.loadingQuery.set(queryKey, promise)
+        return promise
     }
 
     public async get(key: string): Promise<V | undefined> {
@@ -31,7 +80,7 @@ export class CachedLoadableMap<K, V extends Record<string, any>> extends Loadabl
         const now = Date.now()
         if (cacheExpiry) {
             if (cacheExpiry.expiry === -1 || now < cacheExpiry.expiry) {
-                return super.get(key)
+                return Promise.resolve(super.get(key))
             } else {
                 this.expiryCache.delete(key)
                 this.loadOne(key).then((result: V | undefined) => {
@@ -42,10 +91,10 @@ export class CachedLoadableMap<K, V extends Record<string, any>> extends Loadabl
             }
         }
 
+
         if (this.loading.has(key)) {
             return this.loading.get(key)
         }
-
 
         const item = await super.get(key)
         this.setExpiry(key, item)
@@ -60,7 +109,6 @@ export class CachedLoadableMap<K, V extends Record<string, any>> extends Loadabl
 
     public async getBy(propName: string, value: any): Promise<V | undefined> {
         const result = await super.getBy(propName, value)
-
         if (result) {
             this.setExpiry(result[this.keyProperty], result)
         }
@@ -86,8 +134,8 @@ export class CachedLoadableMap<K, V extends Record<string, any>> extends Loadabl
         return result
     }
 
-    public async getAll(): Promise<Map<string, V> | undefined> {
-        const items = await super.getAll()
+    public async getAll(refresh ?: boolean): Promise<Map<string, V> | undefined> {
+        const items = await super.getAll(refresh)
         if (items) {
             items.forEach((_, key) => {
                 this.setExpiry(key, items.get(key))
