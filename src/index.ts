f71fa4cd1024bb61650cb110ce218ef142ebb329
Larry Williamson <l422y@l422y.com>
2024-11-09 23:47:15 -0500

    feat: export CacheItem and FilterableMapOptions types
    Enable external usage of CacheItem and FilterableMapOptions by making them exported in index.ts. This change facilitates better module interoperability.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-type CacheItem<V> = {
+export type CacheItem<V> = {
     value: V,
     expiry: number
 }
 
-type FilterableMapOptions<K, V> = {
+export type FilterableMapOptions<K, V> = {
     filter?: (item: V) => boolean;
     sort?: (item1: V, item2: V) => number;
     searchableFields?: ( keyof V )[];
